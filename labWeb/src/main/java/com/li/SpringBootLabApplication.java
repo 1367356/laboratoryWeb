cf5ed7765cb17aed3c801e84d0e55a07cc3dd27c
1367356 <1185978642@qq.com>
2018-06-20 07:22:02 +0800

    commit
    commit

--- labWeb/src/main/java/com/li/SpringBootLabApplication.java
+++ labWeb/src/main/java/com/li/SpringBootLabApplication.java
@@ -26,5 +26,4 @@ public class SpringBootLabApplication {
     public PlatformTransactionManager txManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
-
 }
